5aafce143246698d34065a0be1410a8a0e177bc0
Lucas Marques <lucasmarquesds@gmail.com>
2014-10-28 21:30:24 -0200

    login template

--- webnews/settings.py
+++ webnews/settings.py
@@ -85,3 +85,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# Additional locations of static files
+STATICFILES_DIRS = (
+    os.path.normpath(os.path.join(BASE_DIR, 'static')),
+    # Put strings here, like "/home/html/static" or "C:/www/django/static".
+    # Always use forward slashes, even on Windows.
+    # Don't forget to use absolute paths, not relative paths.
+)
+
+STATIC_ROOT = BASE_DIR + '/staticroot/'
